ef131dba6229ff8e39d527cda2d16347c1ac8b29
Your Name <youremail@example.com>
2023-06-06 09:52:34 +0800

    INIT 内核端代码

--- MySpace/xdp_prog_kern.c
+++ MySpace/xdp_prog_kern.c
@@ -12,23 +12,6 @@
 #include "../common/xdp_stats_kern.h"
 
 
-static __always_inline __u16 csum_fold_helper(__u32 csum)
-{
-	__u32 sum;
-	sum = (csum >> 16) + (csum & 0xffff);
-	sum += (sum >> 16);
-	return ~sum;
-}
-
-static __always_inline void ipv4_csum(void *data_start, int data_size,
-				      __u32 *csum)
-{
-	*csum = bpf_csum_diff(0, 0, data_start, data_size, *csum);
-    bpf_printk("csum_in %d.\n", *csum);
-	*csum = csum_fold_helper(*csum);
-    bpf_printk("csum_in2 %d.\n", *csum);
-}
-
 SEC("xdp_TTL_rewrite")
 int xdp_TTL_rewrite_func(struct xdp_md *ctx)
 {
@@ -43,10 +26,9 @@ int xdp_TTL_rewrite_func(struct xdp_md *ctx)
     void *data              = (void *)(long)ctx->data;
     struct hdr_cursor nh    = { .pos = data };
 
-    __u32 saddr;
-    __u32 csum = 0;
+    // __u32 saddr;
+    // __u32 csum = 0;
 
-    /*解析以太网包头*/
     eth_type = parse_ethhdr(&nh, data_end, &eth);
 
     bpf_printk("eth_type %d.\n", eth_type);
@@ -55,30 +37,13 @@ int xdp_TTL_rewrite_func(struct xdp_md *ctx)
         goto out;
     }
 
-    // 重构注释的代码
     if (eth_type == bpf_htons(ETH_P_IP)) {
         ip_type = parse_iphdr(&nh, data_end, &iphdp);
-        bpf_printk("iphdr %d.\n", ip_type);
-        /*获取源地址*/
+        bpf_printk("ip_type %d.\n", ip_type);
         if (iphdp) {
-            saddr = iphdp->saddr;
-            bpf_printk("saddr %d.\n", saddr);
-            bpf_printk("htons_saddr %d.\n", bpf_htonl(saddr));
+            // saddr = iphdp->saddr;
             TTL_N = iphdp->ttl;
             bpf_printk("TTL_N %d.\n", TTL_N);
-            iphdp->ttl = 63;
-            bpf_printk("TTL_N %d.\n", iphdp->ttl);
-
-            bpf_printk("iphdp->check %d.\n", iphdp->check);
-
-            ipv4_csum(iphdp, sizeof(struct iphdr), &csum);
-            bpf_printk("csum3 %d.\n", csum);
-            iphdp->check = csum;
-            
-            bpf_trace_printk("iphdp->check %d.\n", iphdp->check);
-            // iphdp->ttl = 64;
-            bpf_printk("TTL_N %d.\n", iphdp->ttl);
-
             action = XDP_PASS;
             goto out;
         }
